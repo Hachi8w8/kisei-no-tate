5d9bf1fe6b9c52172b3f4ca466ca42ac8f5182f0
honey8bee <m8hchi888x@gmail.com>
2025-02-24 19:16:18 +0900

    Simplify build config

--- copy-static.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mkdir -p docs/static
-cp -r docs/_next/static/* docs/static/
--- next.config.js
+++ next.config.js
@@ -1,28 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(woff|woff2|ttf|otf)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          outputPath: 'static/fonts/',
-          publicPath: '/kisei-no-tate/static/fonts/'
-        }
-      }
-    });
-    return config;
-  },
   output: 'export',
   basePath: '/kisei-no-tate',
-  assetPrefix: '/kisei-no-tate',
   images: {
     unoptimized: true,
   },
-  distDir: 'docs',
-  trailingSlash: true
+  distDir: 'docs'
 }
 
 module.exports = nextConfig 
